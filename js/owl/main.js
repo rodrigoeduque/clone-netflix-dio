7ee43b14041f352382f508c64ee067b11b98967e
rodrigoeduque <rodrigoeduque@hotmail.com>
2020-11-23 14:01:36 -0300

    Adicionado Tela Login

--- js/owl/main.js
+++ js/owl/main.js
@@ -16,3 +16,12 @@ $('.owl-carousel').owlCarousel({
 
     }
 })
+
+$('.custom1').owlCarousel({
+    animateOut: 'slideOutDown',
+    animateIn: 'flipInX',
+    items: 1,
+    margin: 30,
+    stagePadding: 30,
+    smartSpeed: 450
+});
